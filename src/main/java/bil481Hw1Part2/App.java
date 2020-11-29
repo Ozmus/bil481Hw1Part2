6d13d5f6efd0a29c16dcc7f9ca5ce6fd6b6243e0
Ozmus <ahmetozmus2018@gmail.com>
2020-11-29 18:46:07 +0300

    Building an web page using spark and mustache. Also, adding apache loggings.

--- src/main/java/bil481Hw1Part2/App.java
+++ src/main/java/bil481Hw1Part2/App.java
@@ -4,13 +4,21 @@
 package bil481Hw1Part2;
 
 import java.util.ArrayList;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 
 
 public class App {
     public static int sumOfNumbersInIndeces(ArrayList<Integer> array, int firstIndex, int secondIndex)throws Exception{
-        System.out.println("inside search");
-
         if (array == null){
             throw new Exception("Array is null.");
         }
@@ -23,12 +31,57 @@ public class App {
       }
 
 
-    public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        for(int i = 0 ; i<10; i++){
-            a.add(i);
-        }
+      public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
-        //System.out.println("sumOfNumbersInIndeces(a, 1, 2): "+sumOfNumbersInIndeces(a, 1, 2));
+        get("/", (req, res) -> "Welcome to Sum Machine!");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          ArrayList<Integer> inputList = new ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          sc1.close();
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          int input3AsInt = Integer.parseInt(input3);
+
+          int result = App.sumOfNumbersInIndeces(inputList, input2AsInt, input3AsInt);
+
+          Map<String, Integer> map = new HashMap<String, Integer>();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map<String, String> map = new HashMap<String, String>();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
